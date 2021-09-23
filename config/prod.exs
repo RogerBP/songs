3986aadb75cd481c077900ffe8037b83d48e3da8
ModeraCelulares <rogerbpessoa@gmail.com>
2021-09-23 13:21:55 -0300

    Heroku - ajustes

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,14 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :songs, SongsWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "agile-sea-80756.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
+
+# url:[scheme:"httpps", host: "agile-sea-80756.herokuapp.com", port:443],
+# force_ssl:[rewrite_on: [:x_forwarded_proto]],
+# secret_key_base: System.getenv("SECRET_KEY_BASE"),
 
 # Do not print debug messages in production
 config :logger, level: :info
--- lib/songs_web/endpoint.ex
+++ lib/songs_web/endpoint.ex
@@ -11,7 +11,8 @@ defmodule SongsWeb.Endpoint do
   ]
 
   socket "/socket", SongsWeb.UserSocket,
-    websocket: true,
+    # websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
